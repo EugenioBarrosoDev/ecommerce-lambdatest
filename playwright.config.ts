402df4e5f0964586edb5c3f0d5b69a6b82201b2a
Eugenio Barroso <eugenio.barroso@miracledevs.com>
2024-03-19 15:05:01 -0300

    compare-buy created, added screenshot on fail

--- playwright.config.ts
+++ playwright.config.ts
@@ -16,7 +16,7 @@ export default defineConfig({
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
-  retries: process.env.CI ? 2 : 0,
+  retries: 2,
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   grepInvert: /login/,
@@ -30,9 +30,10 @@ export default defineConfig({
     headless: false,
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry', 
+    screenshot: 'only-on-failure',
     video: 'retain-on-failure',
-
   },
+  
 
   /* Configure projects for major browsers */
   projects: [
